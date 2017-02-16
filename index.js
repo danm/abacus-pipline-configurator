8369971f236cfb772c379309f0c11a24f3033766
Daniel Morrison <daniel.morrison@bbc.co.uk>
2017-02-16 00:48:15 +0000

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+let cosmosConfig;
+let environment;
+let appConfig;
+
+try {
+    cosmosConfig = require('/config/config.json');
+    environment = cosmosConfig.environment;
+} catch (e) {
+    environment = 'sandbox';
+}
+
+if ('sandbox' === environment) {
+    try {
+        appConfig = require('./configuration-secure.json');
+    } catch (e) {
+        throw new Error('Cannot load configuration file');
+    }
+    appConfig.environment = environment;
+} else {
+    try {
+        appConfig = require('./configuration.json');
+    } catch (e) {
+        throw new Error('Cannot load configuration file');
+    }
+    appConfig.environment = environment;
+    appConfig.cosmos = cosmosConfig;
+}
+
+module.exports = {
+    getConfig: appConfig,
+    getEnvironment: environment
+};
