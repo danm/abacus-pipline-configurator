26c6cc5e50d64d94fb82ee01581acbb1a526d959
Daniel Morrison <daniel.morrison@bbc.co.uk>
2017-03-10 18:57:34 +0000

    tidied up file and allowed for sandbox config file

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 let cosmosConfig;
 let environment;
-let appConfig;
+let appConfig = {};
+
+appConfig.environment = environment;
+appConfig.cosmos = cosmosConfig;
 
 try {
     cosmosConfig = require('/config/config.json');
     environment = cosmosConfig.environment;
+    appConfig.cosmos = cosmosConfig;
+    appConfig.environment = environment;
 } catch (e) {
     environment = 'sandbox';
+    appConfig.environment = environment;
 }
 
-if ('sandbox' === environment) {
-    try {
-        appConfig = require('../../configuration-secure.json');
-    } catch (e) {
-        throw new Error('Cannot load configuration file');
-    }
-    appConfig.environment = environment;
-} else {
-    try {
-        appConfig = require('../../configuration-' + environment + '.json');
-    } catch (e) {
-        throw new Error('Cannot load configuration file');
-    }
-    appConfig.environment = environment;
-    appConfig.cosmos = cosmosConfig;
+try {
+    appConfig = require('../../configuration-' + environment + '.json');
+} catch (e) {
+    throw new Error('Cannot load configuration file');
 }
 
 module.exports = {
